f15f8861450cc37a6807c37e6edbbfe07c4da90c
Aziz Masmoudi <aziz.masmoudi@sesame.com.tn>
2025-05-18 11:42:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
+API_URL = "http://127.0.0.1:5000/predict"
 
 st.set_page_config(page_title="Détection Alzheimer - UI Avancée", layout="wide")
 
